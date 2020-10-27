d0974d1d1c0580fe3adf2315000ecb82b78a685c
Davi Teixeira <daviteixeira.btm@gmail.com>
2020-10-27 02:45:05 -0300

    Implementação do TAD Ponto usando Programação Orientada a Objetos

---
+++ Tópico 6 - Tipos Abstratos de Dados/TAD_Ponto_Orientado_a_ Objetos/main2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Ponto2.h"
+using namespace std;
+
+int main() {
+    Ponto p1 { 2.3, 4.5 };
+    Ponto p2 { 4, 7.8 };
+
+    cout << "Ponto 1: ";
+    cout << "(" << p1.getX() << "," << p1.getY() << ")\n";
+
+    cout << "Ponto 2: ";
+    cout << "(" << p2.getX() << "," << p2.getY() << ")\n";
+
+    cout << "Distancia: " << p1.distancia(p2) << endl;
+    return 0;
+}
